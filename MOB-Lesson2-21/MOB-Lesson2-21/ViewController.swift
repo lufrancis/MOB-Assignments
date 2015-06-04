ecf33ad27fa59d6a46154987558ee92bf8c32a1a
Francis Lu <lufrancis@yahoo.com>
2015-06-04 20:24:37 +0800

    Initial Commit

---
+++ MOB-Lesson2-21/MOB-Lesson2-21/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  MOB-Lesson2-21
+//
+//  Created by Francis Lu on 3/6/15.
+//  Copyright (c) 2015 Francis Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    @IBAction func buttonClicked(sender: AnyObject) {
+        
+        println("hello MOB")
+        println("hello World!")
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
