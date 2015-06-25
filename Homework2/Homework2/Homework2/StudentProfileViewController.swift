4bf05539ebd2dea212f332398bce1cb064c8ae24
Francis Lu <lufrancis@yahoo.com>
2015-06-25 20:09:23 +0800

    Initial Commit for Homework 2

---
+++ Homework2/Homework2/Homework2/StudentProfileViewController.swift
@@ -0,0 +1,37 @@
+//
+//  StudentProfileViewController.swift
+//  Homework2
+//
+//  Created by Kannan Chandrasegaran on 25/6/15.
+//  Copyright (c) 2015 Kannan Chandrasegaran. All rights reserved.
+//
+
+import UIKit
+
+class StudentProfileViewController: UIViewController {
+
+  var student:Student = Student()
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
